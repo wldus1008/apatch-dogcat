9f378b19154b649abce052e5db1649f7182231ef
user <user@DESKTOP-U59IHVT>
2023-01-19 10:21:46 +0900

    수정

--- final/src/main/java/kr/smhrd/dogcat/HomeController.java
+++ final/src/main/java/kr/smhrd/dogcat/HomeController.java
@@ -32,6 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		System.out.println("안녕하세요11");
 		
 		return "home";
 	}
