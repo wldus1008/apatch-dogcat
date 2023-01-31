e40d36fad8a769b93f2fc59b1a9af7fe29011c11
user <user@DESKTOP-U59IHVT>
2023-01-31 14:35:25 +0900

    달력업데이트

---
+++ MVC4/src/main/java/kr/board/controller/BoardController.java
@@ -0,0 +1,184 @@
+package kr.board.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jdk.nashorn.internal.ir.RuntimeNode.Request;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import kr.board.entity.Board;
+import kr.board.entity.Files;
+import kr.board.entity.Member;
+import kr.board.mapper.BoardMapper;
+import kr.board.mapper.imgMapper;
+
+// POJO라는걸 알려주는 주석(어노테이션)
+// 전처리를 하기 위한 기호 : 주석
+// 스프링 프레임워크가 이미 짜여서 있기때문에 그 틀에 맞게 일 시키기 위해서
+// 초창기 개발 @Component
+@Controller
+public class BoardController {
+
+
+	@Autowired
+	private BoardMapper mapper;
+	@Autowired
+	private imgMapper imgMapper;
+	
+	// url 가져오는 방식에 따라 get방식으로 넘어오면
+	// 맵핑을 get으로
+
+	@GetMapping("/basic.do")
+	public String basic() {
+		
+		// 알아서 ViewResolver에 의해
+		// WEB-INF/views에 있는 basic.jsp로 이동
+		
+		return "basic";
+		
+	}
+	
+	
+	@RequestMapping("/login.do")
+	public String login() {
+		
+		// 알아서 ViewResolver에 의해
+		// WEB-INF/views에 있는 basic.jsp로 이동
+		
+		return "login";
+		
+	}
+	
+	@RequestMapping("/register.do")
+	public String register() {
+		
+	
+		
+		return "register";
+		
+	}
+	
+	@RequestMapping("/Home.do")
+	public String Home(Model model, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("loginMember") != null) {
+			Member loginMember = (Member) session.getAttribute("loginMember");
+			System.out.println("id : " + loginMember.getMemId());
+
+			Files img = imgMapper.getImg(loginMember.getMemId());
+			if(img != null) {
+				String fileName = img.getFilename();
+				model.addAttribute("fileName", fileName);
+			}
+			
+		}
+		
+		return "Home";
+		
+	}
+	
+	@RequestMapping("/imgView.do")
+	public String imgView() {
+		
+	
+		
+		return "imgView";
+		
+	}
+	
+
+	@RequestMapping("/goService.do")
+	public String goService() {
+		
+	
+		
+		return "service";
+		
+	}
+	
+	@RequestMapping("/service.do")
+	public String service(Model model, @RequestParam("weight") int weight, @RequestParam("height") int height) {
+		    // 소켓을 선언한다.
+		    try (Socket client = new Socket()) {
+		      // 소켓에 접속하기 위한 접속 정보를 선언한다.
+		      InetSocketAddress ipep = new InetSocketAddress("127.0.0.1", 9999);
+		      // 소켓 접속!
+		      client.connect(ipep);
+		      // 소켓이 접속이 완료되면 inputstream과 outputstream을 받는다.
+		      
+		      try (OutputStream sender = client.getOutputStream(); InputStream receiver = client.getInputStream();) {
+		        // 메시지는 for 문을 통해 10번 메시지를 전송한다.
+		        
+		        // 전송할 메시지를 작성한다.
+		        String msg = weight + "," + height;
+		        // string을 byte배열 형식으로 변환한다.
+		        byte[] data = msg.getBytes();
+		        // ByteBuffer를 통해 데이터 길이를 byte형식으로 변환한다.
+		        ByteBuffer b = ByteBuffer.allocate(4);
+		        // byte포멧은 little 엔디언이다.
+		        b.order(ByteOrder.LITTLE_ENDIAN);
+		        b.putInt(data.length);
+		        // 데이터 길이 전송
+		        sender.write(b.array(), 0, 4);
+		        // 데이터 전송
+		        sender.write(data);
+		          
+		        data = new byte[4];
+		        // 데이터 길이를 받는다.
+		        receiver.read(data, 0, 4);
+		        // ByteBuffer를 통해 little 엔디언 형식으로 데이터 길이를 구한다.
+		        b = ByteBuffer.wrap(data);
+		        b.order(ByteOrder.LITTLE_ENDIAN);
+		        int length = b.getInt();
+		        // 데이터를 받을 버퍼를 선언한다.
+		        data = new byte[length];
+		        // 데이터를 받는다.
+		        receiver.read(data, 0, length);
+		          
+		        // byte형식의 데이터를 string형식으로 변환한다.
+		        msg = new String(data, "UTF-8");
+		        // 콘솔에 출력한다.
+		        System.out.println(msg);
+		        model.addAttribute("bmi", msg);
+		        
+		      }
+		    } catch (Throwable e) {
+		      e.printStackTrace();
+		    }
+		
+		return "serviceResult";
+	}
+	
+	@RequestMapping("/map.do")
+	public String map() {
+		
+	
+		
+		return "map";
+		
+	}
+	
+	
+	
+	
+}
---
+++ MVC4/src/main/java/kr/board/controller/MemberController.java
@@ -0,0 +1,77 @@
+package kr.board.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.board.entity.Member;
+import kr.board.mapper.BoardMapper;
+import kr.board.mapper.MemberMapper;
+import kr.board.mapper.imgMapper;
+
+@Controller
+public class MemberController {
+	
+	@Autowired
+	private MemberMapper mmapper;
+	
+	
+	//회원가입
+		@PostMapping("/join.do")
+		public String register(Member vo){
+			
+			// 회원가입 서비스 실행
+			mmapper.memberJoin(vo);
+			
+			return "Home";
+			
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	@Autowired
+	private BoardMapper mapper;
+	private imgMapper imapper;
+	// private MemberMapper mmapper;
+	
+		
+	// memId, memPw를 담는 Member객체로 묶어 줄 수 있음
+	@PostMapping("/Login.do")
+	public String Login(Member mvo, HttpServletRequest request) {
+		// 로그인 기능 - 해당 아이디, 비밀번호 일치하는 회원의 정보 세션에 저장
+		 Member loginMember = mapper.memberLogin(mvo);
+		
+		 // 만약에 로그인 정보가 있으면 -> 세션에 정보를 저장
+		 if(loginMember != null) {
+			 // 1. 세션 객체 생성
+			 HttpSession session = request.getSession();
+			 // 2. 세션에 값을 setAttrubute()
+			 session.setAttribute("loginMember", loginMember);
+		 }
+		 
+		// basic.jsp로 이동
+		 // 다른 컨트롤러에 있는 메소드 실행
+		return "redirect:/Home.do";
+	}
+	
+	@RequestMapping("/Logout.do")
+	public String Logout(HttpSession session) {
+		session.removeAttribute("loginMember");
+		
+		return "redirect:/Home.do";
+	}
+	
+	
+}
